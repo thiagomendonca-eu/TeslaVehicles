f3bcc0556bd9115593fe1cbbbed08eb2e22d5c8e
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-05-30 16:11:57 -0400

    Update Viewer.vue

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -56,7 +56,7 @@
 export default {
   data() {
     return {
-      veiculos: [],
+      veiculos: null,
       veiculosCopy: [],
       search: "",
       supabaseUrl: import.meta.env.VITE_SUPABASE_URL,
